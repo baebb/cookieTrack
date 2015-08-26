cc1a9bc069a82f51b091d35bcc972769092b6d71
Ross Dyson <dyson.ross@gmail.com>
2015-08-26 04:03:02 -0700

    init commit

---
+++ js/scripty.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $('.but1').click(
+        function () {
+            //first event
+        }
+    );
+    $('.but2').click(
+        function () {
+            //second event
+        }
+    );
+});
